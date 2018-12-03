f2c73e209f88ca25b928ba9eb92bc0cf5eb970e7
Nishan Hossepian <nhossep@gmail.com>
2018-12-03 09:07:48 -0800

    Add files via upload

---
+++ _storybook/webpack.config.js
@@ -0,0 +1,90 @@
+const path = require("path");
+const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
+
+// style files regexes
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+
+const getStyleLoaders = (cssOptions, preProcessor) => {
+  const loaders = [
+    require.resolve("style-loader"),
+    {
+      loader: require.resolve("css-loader"),
+      options: cssOptions
+    },
+    {
+      loader: require.resolve("postcss-loader"),
+      options: {
+        ident: "postcss",
+        plugins: () => [
+          require("postcss-flexbugs-fixes"),
+          require("postcss-preset-env")({
+            autoprefixer: {
+              flexbox: "no-2009"
+            },
+            stage: 3
+          })
+        ]
+      }
+    }
+  ];
+  if (preProcessor) {
+    loaders.push(require.resolve(preProcessor));
+  }
+  return loaders;
+};
+
+module.exports = {
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "awesome-typescript-loader"
+      },
+      {
+        test: cssRegex,
+        exclude: cssModuleRegex,
+        use: getStyleLoaders({
+          importLoaders: 1
+        })
+      },
+      {
+        test: cssModuleRegex,
+        use: getStyleLoaders({
+          importLoaders: 1,
+          modules: true,
+          getLocalIdent: getCSSModuleLocalIdent
+        })
+      },
+      {
+        test: sassRegex,
+        exclude: sassModuleRegex,
+        use: getStyleLoaders({ importLoaders: 2 }, "sass-loader")
+      },
+      {
+        test: sassModuleRegex,
+        use: getStyleLoaders(
+          {
+            importLoaders: 2,
+            modules: true,
+            getLocalIdent: getCSSModuleLocalIdent
+          },
+          "sass-loader"
+        )
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      }
+    ]
+  }
+};
